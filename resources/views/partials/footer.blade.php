2bcffa9dc20ba0b07df5ba5ca68f2558a3054689
stacy_r <stacyrileyca@gmail.com>
2024-08-12 15:24:19 -0500

    Update styles in footer and social media links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <!--============================= FOOTER =============================-->
 <footer>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 p-3 d-flex justify-content-center">
                 <div class="subscribe">
                     <h3>Newsletter</h3>
                     <form id="subscribeform" action="../public/assets/php/twitter/subscribe.php" method="post">
                         <input class="signup_form" type="text" name="email" placeholder="Enter Your Email Address">
-                        <button type="submit" class="btn btn-warning" id="js-subscribe-btn">SUBSCRIBE</button>
+                        <button type="submit" class="btn btn-warning" id="js-subscribe-btn">Subscribe</button>
                         <div id="js-subscribe-result" data-success-msg="Success, Please check your email." data-error-msg="Oops! Something went wrong"></div>
                         <!-- // end #js-subscribe-result -->
                     </form>
@@ -16,7 +16,7 @@
         </div>
 
         <div class="row justify-content-between">
-            <div class="col-md-3 d-flex justify-content-center">
+            <div class="col-md-4 d-flex justify-content-center">
                 <div class="sitemap">
                     <h3>Navigation</h3>
                     <ul>
@@ -26,8 +26,8 @@
                                 Services
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/services/education">Education Services</a></li>
-                                <li><a class="dropdown-item" href="/services/corporate">Corporate Services</a></li>
+                                <li><a class="dropdown-item dropdown-item-footer" href="/services/education">Education Services</a></li>
+                                <li><a class="dropdown-item dropdown-item-footer" href="/services/corporate">Corporate Services</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
@@ -41,10 +41,10 @@
                                 Media &amp; Partners
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/community_center">Learning &amp; Community Center</a></li>
-                                <li><a class="dropdown-item" href="/partners">Partners</a></li>
-                                <li><a class="dropdown-item" href="/press">Press</a></li>
-                                <li><a class="dropdown-item" href="/blog">Blog</a></li>
+                                <li><a class="dropdown-item dropdown-item-footer" href="/community_center">Learning &amp; Community Center</a></li>
+                                <li><a class="dropdown-item dropdown-item-footer" href="/partners">Partners</a></li>
+                                <li><a class="dropdown-item dropdown-item-footer" href="/press">Press</a></li>
+                                <li><a class="dropdown-item dropdown-item-footer" href="/blog">Blog</a></li>
                             </ul>
                         </li>
                         <li class="">
@@ -55,9 +55,9 @@
                 </div>
             </div>
 
-            <div class="col-md-5">
-                <h3 style="color: white;" class="text-center">Contact Us</h3>
-                <p style="color: white;" class=" pb-4 text-center">If you aren't able find the answers you need, or if you have a general question,
+            <div class="col-md-4 contact-us-container">
+                <h3 class="text-center">Contact Us</h3>
+                <p class=" pb-4 text-center">If you aren't able find the answers you need, or if you have a general question,
                     feel free to contact us. We’ll get back to you within 24–48 hours.
                 </p>
                 <div class="container-custom-link-styles-footer button-secondary-cta">
@@ -66,21 +66,34 @@
                 </div>
             </div>
 
-            <div class="col-md-3 d-flex justify-content-center">
-                    <div class="text-center pl-2 pr-2 ">
-                        <h3 style="color: white;">House of Light</h3>
-                        <p style="color: white;"><span>Address: </span> Unisco university Albany, NY, USA. 11001</p>
+            <div class="col-xs-6 col-md-4 d-flex justify-content-center address-container">
+                    <div class="text-left pl-2 pr-2 ">
+                        <h3>House of Light</h3>
+                        <p><span >Address: </span><br>6815 W 167th Street Suite B
+                            <br>Tinley Park, IL 60426 </p>
                         <br >
-                        <p style="color: white;">Phone : +91 555 668 986</p>
+                        <p>Phone : (708) 894-2150</p>
                         <ul class="footer-social-icons p-0">
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fa fa-youtube fa-yt" aria-hidden="true"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.facebook.com/houseoflightllc"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.instagram.com/house_of_lightllc"><i class="fa fa-instagram fa-ig" aria-hidden="true"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.tiktok.com/@house_of_lightllc">
+                                    <svg class="fa-tt" fill="#000000" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M412.19,118.66a109.27,109.27,0,0,1-9.45-5.5,132.87,132.87,0,0,1-24.27-20.62c-18.1-20.71-24.86-41.72-27.35-56.43h.1C349.14,23.9,350,16,350.13,16H267.69V334.78c0,4.28,0,8.51-.18,12.69,0,.52-.05,1-.08,1.56,0,.23,0,.47-.05.71,0,.06,0,.12,0,.18a70,70,0,0,1-35.22,55.56,68.8,68.8,0,0,1-34.11,9c-38.41,0-69.54-31.32-69.54-70s31.13-70,69.54-70a68.9,68.9,0,0,1,21.41,3.39l.1-83.94a153.14,153.14,0,0,0-118,34.52,161.79,161.79,0,0,0-35.3,43.53c-3.48,6-16.61,30.11-18.2,69.24-1,22.21,5.67,45.22,8.85,54.73v.2c2,5.6,9.75,24.71,22.38,40.82A167.53,167.53,0,0,0,115,470.66v-.2l.2.2C155.11,497.78,199.36,496,199.36,496c7.66-.31,33.32,0,62.46-13.81,32.32-15.31,50.72-38.12,50.72-38.12a158.46,158.46,0,0,0,27.64-45.93c7.46-19.61,9.95-43.13,9.95-52.53V176.49c1,.6,14.32,9.41,14.32,9.41s19.19,12.3,49.13,20.31c21.48,5.7,50.42,6.9,50.42,6.9V131.27C453.86,132.37,433.27,129.17,412.19,118.66Z"/>
+                                    </svg>
+                                </a>
+                                </li>
+
+                            <li class="list-inline-item"><a href="https://www.linkedin.com/company/house-of-light-llc"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-center footer-copyright-container">
+                    <p class="p-0">&copy; House of Light, LLC </p>
+                </div>
+            </div>
+        </div>
 </footer>
