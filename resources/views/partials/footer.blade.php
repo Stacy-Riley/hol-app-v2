f10d99d72c9dd17253dc51405eab29c44bd3d73f
stacy_r <stacyrileyca@gmail.com>
2024-09-03 13:06:06 -0500

    Add a home navigation tag to footer, add aria labels(HOLV2-23)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -11,11 +11,11 @@
                         <button type="submit" class="btn btn-warning">Subscribe</button>
                     </form>
                     @if(session('success'))
-                        <p>{{ session('success') }}</p>
+                        <p role="alert">{{ session('success') }}</p>
                     @endif
 
                     @if(session('error'))
-                        <p>{{ session('error') }}</p>
+                        <p role="alert">{{ session('error') }}</p>
                     @endif
 
                 </div>
@@ -27,6 +27,7 @@
                 <div class="sitemap">
                     <h3>Navigation</h3>
                     <ul>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                         <li class="nav-item dropdown">
                             <a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -62,14 +63,14 @@
                 </div>
             </div>
 
-            <div class="col-md-4 contact-us-container">
+            <div class="col-md-4 px-2 contact-us-container">
                 <h3 class="text-center">Contact Us</h3>
-                <p class=" pb-4 text-center">If you aren't able find the answers you need, or if you have a general question,
+                <p class="pb-4 text-center">If you aren't able find the answers you need, or if you have a general question,
                     feel free to contact us. We’ll get back to you within 24–48 hours.
                 </p>
                 <div class="container-custom-link-styles-footer button-secondary-cta">
-                    <a href="/faq" >FAQ</a>
-                    <a href="/contact" >Contact</a>
+                    <a href="/faq">FAQ</a>
+                    <a href="/contact">Contact</a>
                 </div>
             </div>
 
@@ -78,18 +79,19 @@
                         <h3>House of Light</h3>
                         <p><span >Address: </span><br>6815 W 167th Street Suite B
                             <br>Tinley Park, IL 60426 </p>
-                        <br >
-                        <p>Phone : (708) 894-2150</p>
+
+                        <p class="pb-2">Phone : (708) 894-2150</p>
+
                         <ul class="footer-social-icons p-0">
-                            <li class="list-inline-item"><a href="https://www.facebook.com/houseoflightllc"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
-                            <li class="list-inline-item"><a href="https://www.instagram.com/house_of_lightllc"><i class="fa fa-instagram fa-ig" aria-hidden="true"></i></a></li>
-                            <li class="list-inline-item"><a href="https://www.tiktok.com/@house_of_lightllc">
+                            <li class="list-inline-item"><a href="https://www.facebook.com/houseoflightllc" aria-label="Visit our Facebook page"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.instagram.com/house_of_lightllc" aria-label="Visit our Instagram page"><i class="fa fa-instagram fa-ig" aria-hidden="true"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.tiktok.com/@house_of_lightllc" aria-label="Visit our Tiktok page">
                                     <svg class="fa-tt" fill="#000000" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M412.19,118.66a109.27,109.27,0,0,1-9.45-5.5,132.87,132.87,0,0,1-24.27-20.62c-18.1-20.71-24.86-41.72-27.35-56.43h.1C349.14,23.9,350,16,350.13,16H267.69V334.78c0,4.28,0,8.51-.18,12.69,0,.52-.05,1-.08,1.56,0,.23,0,.47-.05.71,0,.06,0,.12,0,.18a70,70,0,0,1-35.22,55.56,68.8,68.8,0,0,1-34.11,9c-38.41,0-69.54-31.32-69.54-70s31.13-70,69.54-70a68.9,68.9,0,0,1,21.41,3.39l.1-83.94a153.14,153.14,0,0,0-118,34.52,161.79,161.79,0,0,0-35.3,43.53c-3.48,6-16.61,30.11-18.2,69.24-1,22.21,5.67,45.22,8.85,54.73v.2c2,5.6,9.75,24.71,22.38,40.82A167.53,167.53,0,0,0,115,470.66v-.2l.2.2C155.11,497.78,199.36,496,199.36,496c7.66-.31,33.32,0,62.46-13.81,32.32-15.31,50.72-38.12,50.72-38.12a158.46,158.46,0,0,0,27.64-45.93c7.46-19.61,9.95-43.13,9.95-52.53V176.49c1,.6,14.32,9.41,14.32,9.41s19.19,12.3,49.13,20.31c21.48,5.7,50.42,6.9,50.42,6.9V131.27C453.86,132.37,433.27,129.17,412.19,118.66Z"/>
                                     </svg>
                                 </a>
                                 </li>
-                            <li class="list-inline-item"><a href="https://www.linkedin.com/company/house-of-light-llc"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
+                            <li class="list-inline-item"><a href="https://www.linkedin.com/company/house-of-light-llc" aria-label="Visit our LinkedIn page"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
                         </ul>
                     </div>
                 </div>
